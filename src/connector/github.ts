eaa6689c573322f5ab9735622cdea80fe3e851fd
Adrián Insua Yañez <adrianiy@ext.inditex.com>
2021-10-16 16:21:21 +0200

    chore: revert changes

--- src/connector/github.ts
+++ src/connector/github.ts
@@ -6,10 +6,11 @@ import { LabelNode, PullRequest, PullRequestResponse } from './models/pullReques
 import { Release } from './models/release';
 import { gitHubConnection } from 'connections/github';
 import { CliParams } from 'commander/options';
-import { exec } from 'child_process';
 import prQuery from './queries/pull_requests.graphql';
 import releaseQuery from './queries/latest_release.graphql';
 import log4js from 'log4js';
+import fs from 'fs';
+import path from 'path';
 
 interface Edge<T> {
     cursor: string;
@@ -73,8 +74,9 @@ export class GitHubConnector extends Connector {
 
     async publishChanges(file: string): Promise<void> {
         const filePath = file.replace('./', '');
+        const sha = await this._getSha(filePath);
 
-        await this._publishCommit(filePath);
+        await this._publishCommit(filePath, sha);
     }
 
     protected _setRepoData(repository: string): void {
@@ -107,14 +109,37 @@ export class GitHubConnector extends Connector {
         return this._paginatedResponse<T>(query, params, response);
     }
 
-    private _publishCommit(filePath: string): number {
+    private async _getSha(filePath: string): Promise<string | undefined> {
+        try {
+            const result = (await this._connection.rest.repos.getContent({
+                owner: this._owner,
+                repo: this._repo,
+                path: filePath,
+            })) as ShaResponse;
+
+            const sha = result.data?.sha;
+
+            return sha;
+        } catch (_) {
+            return undefined;
+        }
+    }
+
+    private async _publishCommit(filePath: string, sha?: string): Promise<number> {
         this._verbose && logger.info('We are going to commit changes...');
 
-        exec(`git add ${filePath} && git commit -m "${this._configuration.message}" --no-verify && git push && git pull`, error => {
-            logger.error(error);
-            return 0;
+        const { branch } = this._configuration;
+        const content = fs.readFileSync(path.join(filePath), { encoding: 'base64' });
+        const result = await this._connection.rest.repos.createOrUpdateFileContents({
+            owner: this._owner,
+            repo: this._repo,
+            path: filePath,
+            message: this._configuration.message!,
+            content,
+            sha,
+            branch,
         });
 
-        return 200;
+        return result.status;
     }
 }
