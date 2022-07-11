8051d62d3f9fe4f5fcd366f2c815c0ecdbef3c1c
adrianiy <adrianiy@ext.inditex.com>
2022-07-11 15:15:16 +0200

    feat: add version folder replacement

--- src/generator/github.ts
+++ src/generator/github.ts
@@ -14,7 +14,8 @@ export class GithubGenerator extends Generator {
 
         const oldFile = this._configuration.split ? '' : this._loadMarkdown();
         const notes = this._sortPullRequestByType(pullRequests).map(this._composeText);
-        const header = this._configuration.header?.length ? `${this._configuration.header.replace('${version}', suffix!)}\n` : '';
+        const folder = `v${suffix?.split('.')[0]}`;
+        const header = this._configuration.header?.length ? `${this._configuration.header.replace('${versionFolder}', folder).replace('${version}', suffix!)}\n` : '';
         const title = this._configuration.title?.length ? `# ${this._configuration.title.replace('${version}', suffix!)}\n` : '';
         const markdown = [header, title, ...notes, oldFile].join('\n');
 
